1430e67ad617aabf8cf3b9b07db5f5dae2bf8005
bhatnagaranshika02 <bhatnagaranshika02@gmail.com>
2020-05-24 20:23:57 +0530

    Using python

---
+++ Tree/Height.py
@@ -0,0 +1,52 @@
+class Node:
+    def __init__ (self,node):
+        self.node=node
+        self.left=None
+        self.right=None
+
+class binary:
+    def __init__ (self):
+        self.root=None
+
+    def create(self,val):
+        if self.root==None:
+            self.root=Node(val)
+        else:
+            current=self.root
+            while True:
+                if val<current.node:
+                    if current.left:
+                        currnet=current.left
+                    else:
+                        current.left=Node(val)
+                        break
+                elif val > current.node:
+                    if current.right:
+                        current=current.right
+                    else:
+                        current=Node(val)
+                        break
+                else:
+                    break
+
+    def height(root):
+        if root==None:
+            return -1
+        else:
+            left=height(root.left)
+            right=height(root.right)
+            
+            return 1+ max(left,right)
+
+
+
+tree = binary()
+t = int(input())
+
+arr = list(map(int, input().split()))
+
+for i in range(t):
+    tree.create(arr[i])
+
+p=height(tree.root)
+print(p)
---
+++ Tree/LowestCommonAncester.py
@@ -0,0 +1,9 @@
+def lca(root, v1, v2):
+    while(root != None):
+        if(v1 > root.info and v2 > root.info):
+            root = root.right
+        elif(v1 < root.info and v2 < root.info):
+            root = root.left
+        else:
+            break
+    return root
