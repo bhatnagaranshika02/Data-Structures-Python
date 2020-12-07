442f7dacd27ad28938ac29a7e0b394efde4f4f4a
bhatnagaranshika02 <bhatnagaranshika02@gmail.com>
2020-12-07 09:24:12 +0530

    new

---
+++ algorithm/activitySeletion.py
@@ -0,0 +1,13 @@
+def Activity(s,f):
+	n=len(f)
+	j=0
+	print(j)
+	for i in xrange(n):
+		if s[i]>=f[j]:
+			print(i)
+			j=i
+
+s = [1 , 3 , 0 , 5 , 8 , 5] 
+f = [2 , 4 , 6 , 7 , 9 , 9] 
+Activity(s,f)
+
